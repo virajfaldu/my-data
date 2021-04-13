9295c045c7f74b920e439c02afd94d68aa6739a6
virajfaldu <virajfaldu672002@gmail.com>
2021-04-13 18:02:09 +0530

    .

---
+++ Extra/Django/Project-1/accounts/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+def home(request):
+    return HttpResponse("home")
+
+def customer(request):
+    return render(request,'account/customer.html')
+
+def dashboard(request):
+    return render(request,'account/dashboard.html')
+
+def product(request):
+    return render(request,'account/product.html')
