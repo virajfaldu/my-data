e394b29448c69c6a088341f6d673fd4b517a1af2
virajfaldu <51791964+virajfaldu@users.noreply.github.com>
2021-07-05 15:36:24 +0530

    Delete Extra/Django directory

--- Extra/Django/Learn/cards/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class cards(models.Model):
-    img = models.ImageField(upload_to='media')
-    title =models.CharField(max_length=40)
-    des = models.TextField()
